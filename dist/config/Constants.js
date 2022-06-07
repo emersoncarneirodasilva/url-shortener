6f1498c727de9d283612d5f2a249a1c3ba77cc28
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-06-07 19:55:20 -0300

    Subindo projeto

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: process.env.API_URL || 'http://localhost:5000',
+    MONGO_CONNECTION: process.env.MONGO_CONNECTION
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: process.env.API_URL || "http://localhost:5000",
+	MONGO_CONNECTION: process.env.MONGO_CONNECTION		
+};
+
+// 'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
